ed0ea1f5d592348c3f3d225bb82581219ab3ea7d
ayuen <ayuen@redhat.com>
2019-07-08 09:22:57 +1000

    Updated script

--- initDevice.sh
+++ initDevice.sh
@@ -2,7 +2,7 @@
 
 
 
-USER='ayuen:Wky16694'
+USER='user:password'
 export BASEURL="http://localhost:8080/kie-server/services/rest/server/containers/instances/Demo1"
 
 curl -X POST -H 'X-KIE-ContentType: JSON' -H 'Content-type: application/json' -u $USER \
